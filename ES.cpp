9ae9b62226bf07d102ade33511f773427cdfddf9
fredvollmer <fredric.vollmer@gmail.com>
2015-11-01 12:37:48 -0700

    Getting there...

---
+++ ES.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Fred Vollmer on 11/1/15.
+//
+
+#include "ES.h"
---
+++ ES.h
@@ -0,0 +1,14 @@
+//
+// Created by Fred Vollmer on 11/1/15.
+//
+
+#ifndef ASSIGNMENT3_ES_H
+#define ASSIGNMENT3_ES_H
+
+
+class ES {
+
+};
+
+
+#endif //ASSIGNMENT3_ES_H
